5c04f45fa22e86000d3f202e45a1fab2cb7141aa
tobiwalter <tobi_ggglingen@yahoo.com>
2020-04-29 13:06:07 +0200

    change function back to do all the processing

--- process_protocols.py
+++ process_protocols.py
@@ -18,6 +18,7 @@ logging.info('Lemmatizer and spell checker loaded.')
 
 tpath = os.path.abspath(os.path.join(ROOT_DIR, "data"))
 os.chdir(tpath)
+print(os.getcwd())
 
 # Add and delete certain dictionary entries 
 # TO-DO: load words from txt-list
@@ -39,25 +40,37 @@ class ProcessProtocols(object):
     def process_and_save(self):
         logging.info('Start processing of files.')
         i = 0
-        files_total = len(os.listdir(self.dirname))
-        logging.info(f'{files_total} files were found.')
-        border = round(files_total / 10)
-        for num in range(1,files_total+1):
-            if not os.path.exists(os.join(self.dirname, f'{num}_sents.txt')):
+        files_total = os.listdir(self.dirname)
+        logging.info(f'{len(files_total)} files were found.')
+        border = round(len(files_total) / 10)
+        for num, file in enumerate(files_total):
+            if not os.path.isfile(os.path.join(f'{self.dirname}_processed' , f'{num+1}_sents.txt')):
                 try:
-                    text = open(os.path.join(self.dirname, f'{num}_sents.txt'), encoding='utf-8').readlines()
-                    text = remove_punctuation(text)
+                    text = open(os.path.join(self.dirname,file), 'r', encoding='utf-8').readlines()
+                    # text = remove_punctuation(text)
+                    # text = remove_double_spaces(text)
+                    # text = extract_protocol(text)
+                    text = remove_linebreaks(text)
+                    text = remove_noisy_digits(text)
+                    text = replace_digits(text)
                     text = remove_double_spaces(text)
-                    text = extract_protocol(text)
-                    with open('f{self.dirname}_processed/{num}_sents.txt', 'w') as out_file:
-                        for line in text:
-                            f.write(line)
-                            f.write('\n')
+                    text = reduce_numerical_sequences(text)
+                    text = remove_dash_and_minus_signs(text)
+                    text = filter_lines(text)
+                    text = [removeGermanChainWords(line) for line in text]
+                    text = [remove_hyphens_pre_and_appending(line) for line in text]
+                    text = [lemmatizer.lemmatize(line) for line in text]
+                    text = [lowercase(line) for line in text]
+                    text = [spell_checker.correct(line) for line in text]
 
-                    # save_as_line_sentence(text, f'{self.dirname}_processed/{num}_sents.txt')
+                    # with open(os.path.join(f'{self.dirname}_processed' , f'{num+1}_sents.txt'), 'w', encoding='utf-8') as out_file:
+                    #     for line in text:
+                    #         out_file.write(line)
+                    #         out_file.write('\n')
+                    save_as_line_sentence(text, f'{self.dirname}_processed/{num}_sents.txt')
                     i += 1
                     if i % border == 0:
-                      logging.info('Processing {:03.1f} percent finished'.format(int((i/(files_total)) * 100)))
+                      logging.info('Processing {:03.1f} percent finished'.format(int((i/len(files_total)) * 100)))
 
                 except FileNotFoundError:
                     print(f'File {num} was not found.')
