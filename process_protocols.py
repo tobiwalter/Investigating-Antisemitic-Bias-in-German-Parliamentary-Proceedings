55337a13e5f34408cd4c4f81dd0d058eb9bb2ab4
tobiwalter <tobi_ggglingen@yahoo.com>
2020-04-29 17:08:35 +0200

    correct error that did not read in files in sorted order

--- process_protocols.py
+++ process_protocols.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os 
+import glob
 from gensim.utils import save_as_line_sentence
 from text_preprocessing import *
 import logging
@@ -12,13 +13,11 @@ ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 # import char_split
 lemmatizer = GermanLemmatizer()
 # Initialise spelling correction instance 
-print(ROOT_DIR)
 spell_checker = GermanSpellChecker('dictionaries/de_full.txt')
 logging.info('Lemmatizer and spell checker loaded.')
 
 tpath = os.path.abspath(os.path.join(ROOT_DIR, "data"))
 os.chdir(tpath)
-print(os.getcwd())
 
 # Add and delete certain dictionary entries 
 # TO-DO: load words from txt-list
@@ -40,13 +39,17 @@ class ProcessProtocols(object):
     def process_and_save(self):
         logging.info('Start processing of files.')
         i = 0
-        files_total = os.listdir(self.dirname)
-        logging.info(f'{len(files_total)} files were found.')
-        border = round(len(files_total) / 10)
-        for num, file in enumerate(files_total):
-            if not os.path.isfile(os.path.join(f'{self.dirname}_processed' , f'{num+1}_sents.txt')):
+        files_total = len(os.listdir(self.dirname))
+        logging.info(f'{files_total} files were found.')
+        border = round(files_total / 10)
+        for num in range(1,files_total+1):
+            if not os.path.isfile(os.path.join(f'{self.dirname}_processed' , f'{num}_sents.txt')):
+                # try:
+                #     file = glob.glob(f'{self.dirname}/{num}-*.*.*.txt')[0]
+                # except IndexError:
+                #     print('File not found.')
                 try:
-                    text = open(os.path.join(self.dirname,file), 'r', encoding='utf-8').readlines()
+                    text = open(os.path.join(self.dirname, f'{num}_sents.txt'),'r', encoding='utf-8').readlines()
                     # text = remove_punctuation(text)
                     # text = remove_double_spaces(text)
                     # text = extract_protocol(text)
@@ -63,14 +66,14 @@ class ProcessProtocols(object):
                     text = [lowercase(line) for line in text]
                     text = [spell_checker.correct(line) for line in text]
 
-                    # with open(os.path.join(f'{self.dirname}_processed' , f'{num+1}_sents.txt'), 'w', encoding='utf-8') as out_file:
+                    # with open(os.path.join(f'{self.dirname}_processed' , f'{num}_sents.txt'), 'w', encoding='utf-8') as out_file:
                     #     for line in text:
                     #         out_file.write(line)
                     #         out_file.write('\n')
                     save_as_line_sentence(text, f'{self.dirname}_processed/{num}_sents.txt')
                     i += 1
                     if i % border == 0:
-                      logging.info('Processing {:03.1f} percent finished'.format(int((i/len(files_total)) * 100)))
+                      logging.info('Processing {:03.1f} percent finished'.format(int((i/files_total) * 100)))
 
                 except FileNotFoundError:
                     print(f'File {num} was not found.')
