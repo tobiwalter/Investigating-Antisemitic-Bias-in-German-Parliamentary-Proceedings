2890dbe0f5638924f94ba07dad8005ae0ef55de0
tobiwalter <tobi_ggglingen@yahoo.com>
2020-05-04 20:28:06 +0200

    correct word list creation script

--- create_wordlist.py
+++ create_wordlist.py
@@ -1,9 +1,12 @@
 # Create a word list from the Hunspell dictionary 
 
-with open('dictionaries\\ger-wordlist', 'w') as fout:	
-	with open('dictionaries\\hunspell_de.dic', 'r') as fin:
+with open('dictionaries\\ger-wordlist.txt', 'w') as fout:	
+	with open('dictionaries\\de_DE\\German_de_DE.dic', 'r') as fin:
 		for line in fin:
-			word = line.split('/')[0]
-			fout.write(word)
-			fout.write('\n')
-
+			if '/' in line:
+				word = line.split('/')[0]
+				fout.write(word)
+				fout.write('\n')
+			else:
+				fout.write(line)
+	
