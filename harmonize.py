d26943e1ef2be90627c1b2000aec8c57d23306e0
tobiwalter <tobi_ggglingen@yahoo.com>
2020-05-23 09:22:05 +0200

    add function to harmonize spelling

---
+++ harmonize.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import sys
+import os 
+import glob
+from gensim.utils import save_as_line_sentence
+from text_preprocessing import *
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
+
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+tpath = os.path.abspath(os.path.join(ROOT_DIR, "data"))
+os.chdir(tpath)
+
+
+def harmonize(dirname):
+    logging.info('Start processing of files.')
+    i = 0
+    files_total = len(os.listdir(dirname))
+    logging.info(f'{files_total} files were found.')
+    border = round(files_total / 10)
+    for num in range(1,files_total+1):
+        try:
+            text = open(os.path.join(dirname, f'{num}_sents.txt'),'r', encoding='utf-8').readlines()
+            text = [harmonizeSpelling(line) for line in text]
+            save_as_line_sentence(text, f'{dirname}_harmonized/{num}_sents.txt')
+            i += 1
+            if i % border == 0:
+              logging.info('Processing {:03.1f} percent finished'.format(int((i/files_total) * 100)))
+
+        except FileNotFoundError:
+            print(f'File {num} was not found.')
+
+if __name__ == "__main__":
+  try:
+    dirname = sys.argv[1]
+  except IndexError as e: 
+    print(e)
+
+process_and_save(dirname)
+
+
