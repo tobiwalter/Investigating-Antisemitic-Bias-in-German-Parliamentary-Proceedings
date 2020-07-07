4fc817f74cfc2e010013b95009a40b51477ba36a
tobiwalter <tobi_ggglingen@yahoo.com>
2020-07-07 11:48:14 +0200

    add function that can reindex matrix and index to compute scores for other attribute sets

--- propagate.py
+++ propagate.py
@@ -11,9 +11,10 @@ logging.basicConfig(level=logging.INFO)
 
 M = 1e-06
 class LabelPropagation:
-  def __init__(self, ppmi_mat, tok2indx):
+  def __init__(self, ppmi_mat, tok2indx, protocol_type):
     self.ppmi = ppmi_mat
     self.index = tok2indx
+    self.attributes = create_attribute_sets(self.index, kind=protocol_type)
     self.labels = None
     self.scores = None
   @classmethod
@@ -22,6 +23,25 @@ class LabelPropagation:
     logging.info(f'Shape of PPMI matrix:{ppmi_mat.shape}')
     indx2tok = json.load(open(indx_path, 'r', encoding='utf-8'))
     return cls(ppmi_mat, indx2tok)
+
+  def reindex(self, pos_att, neg_att):
+    pos_indices_to_swap = [self.index[word] for word in attributes[pos_att]]
+    neg_indices_to_swap = [self.index[word] for word in attributes[neg_att]]
+
+    matrix = self.ppmi.toarray()
+    i2t = {v:k for k,v in self.index}
+    for indx_old, indx_new in enumerate(pos_indices_to_swap):
+        matrix[[indx_old, indx_new]] = matrix[[indx_new, indx_old]]
+        matrix[:,[indx_old, indx_new]] = matrix[:,[indx_new, indx_old]]
+        i2t[indx_old], i2t[indx_new] = i2t[indx_new], i2t[indx_old]
+
+    for indx_old, indx_new in enumerate(neg_indices_to_swap):
+        matrix[[indx_old+len(pos_indices_to_swap), indx_new]] = matrix[[indx_new, indx_old+len(pos_indices_to_swap)]]
+        matrix[:,[indx_old+len(pos_indices_to_swap), indx_new]] = matrix[:,[indx_new, indx_old+len(pos_indices_to_swap)]]
+        i2t[indx_old+len(pos_indices_to_swap)], i2t[indx_new] = i2t[indx_new], i2t[indx_old+len(pos_indices_to_swap)]
+        
+    self.index = {v:k for k,v in i2t.items()}
+    self.ppmi = sparse.csr_matrix(matrix)
 
   def create_labels(self, targets_1, targets_2):
     labels = []
@@ -44,10 +64,10 @@ class LabelPropagation:
     self.scores = (-1* inverse(L_uu + np.eye(L_uu.shape[0])*M)).dot(L_ul).dot(self.labels)
     # np.save(f'f_u_scores/{label}', f_u)
 
-  def save_scores(self, path):
+  def save_scores(self, path, attribute_specification):
     if not os.path.exists('fu_scores'):
       os.makedirs('fu_scores')
-    np.save(f'fu_scores/{path}.npy', self.scores)
+    np.save(f'fu_scores/{path}_{attribute_specification}.npy', self.scores)
 
   def get_bias_term_indices(self, targets):
      return {k: [self.index[word] - len(self.labels) for word in v] for k,v in targets.items()}
@@ -83,16 +103,19 @@ def main():
 
   args = parser.parse_args()
   
-  lp = LabelPropagation.load(args.ppmi, args.index)
-  attributes = create_attribute_sets(lp.index, kind=args.protocol_type)
+  lp = LabelPropagation.load(args.ppmi, args.index, kind=args.protocol_type)
+  # attributes = create_attribute_sets(lp.index, kind=args.protocol_type)
   att_1, att_2 = convert_attribute_set(args.attribute_specifications)
   lp.create_labels(attributes[att_1], attributes[att_2])
   print(lp.labels)
+  if attribute_specifications != 'sentiment':
+    logging.info('Reindex matrix')
+    lp.reindex(att_1, att_2)
   start = time.time()
   logging.info(f'Start label propagation for attributes {att_1} and {att_2}')
   lp.propagate()
   #lp.load_scores('fu_scores/kaiserreich_1.npy')
-  lp.save_scores(args.output_file)
+  lp.save_scores(args.output_file, args.attribute_specifications)
   elapsed = time.time()
   logging.info(f'Label propagation finished. Took {(elapsed - start) / 60} min.')
   targets = create_target_sets(lp.index, kind=args.protocol_type)
